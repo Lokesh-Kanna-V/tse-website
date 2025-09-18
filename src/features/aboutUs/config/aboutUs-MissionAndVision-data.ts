78b33b07bc4a7e54f8f35cad56418351cb2db4fc
Lokesh-Kanna-V <vlokeshkanna@gmail.com>
2025-09-18 16:44:18 +0530

    LK_1809_1642

--- src/features/aboutUs/config/aboutUs-MissionAndVision-data.ts
+++ src/features/aboutUs/config/aboutUs-MissionAndVision-data.ts
@@ -5,12 +5,12 @@ export const missionAndVision = [
     title: "Mission",
     icon: Target,
     content:
-      "To make every milestone, celebration, and living space more memorable by providing thoughtfully curated, sustainable, and affordable products that bring comfort, joy, and style to families worldwide.",
+      "Our mission is to make every milestone, celebration, and living space more memorable by providing thoughtfully curated, sustainable, and affordable products that bring comfort, joy, and style to families worldwide.",
   },
   {
     title: "Vission",
     icon: Eye,
     content:
-      "To become a globally trusted brand in curated baby essentials, personalized gifting, and eco-friendly home décor by bridging the gap between skilled suppliers and discerning customers.",
+      "Our vision is to become a globally trusted brand in curated baby essentials, personalized gifting, and eco-friendly home décor by bridging the gap between skilled suppliers and discerning customers.",
   },
 ];
